363d5aa6e7f890e3decc6467c636c48d6d924401
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-01 13:44:58 +0500

    header media the end

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // burger
 let menu_burger = document.querySelector('.menu_burger');
 let mobile_link = document.querySelector('.mobile_link');
+let mobile_menu = document.querySelector('.mobile_menu');
 
 menu_burger.addEventListener('click', () => {
     menu_burger.classList.toggle('active')
     mobile_link.classList.toggle('active')
+    mobile_menu.classList.toggle('active')
 })
 // burger
