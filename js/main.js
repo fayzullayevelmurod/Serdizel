497a8df23fda6e6fe31a24f30959d202e8ab9efc
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-01 16:33:25 +0500

    partner section desktop the end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// partnerslider
+var swiper = new Swiper(".partnerSwiper", {
+    pagination: {
+        el: ".partner-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        360: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        650: {
+            slidesPerView: 5,
+            spaceBetween: 12,
+        },
+        1024: {
+            slidesPerView: 6,
+            spaceBetween: 20,
+        },
+    },
+});
+// partnerslider
+
 // burger
 let menu_burger = document.querySelector('.menu_burger');
 let mobile_link = document.querySelector('.mobile_link');
